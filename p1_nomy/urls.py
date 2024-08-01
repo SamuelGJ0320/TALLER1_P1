94f83b88e49aaca176484d23f3aa6232148d67f6
Samuel Gutierrez Jaramillo <sgutierr28@eafit.edu.co>
2024-07-31 23:52:18 -0500

    busqueda de peliculas

--- p1_nomy/urls.py
+++ p1_nomy/urls.py
@@ -18,7 +18,13 @@ from django.contrib import admin
 from django.urls import path
 from nomy import views  as nomyViews
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', nomyViews.home),
+    path('about/', nomyViews.about),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
